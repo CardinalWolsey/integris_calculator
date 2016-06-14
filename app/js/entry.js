7042b5f197625881f9995eae5271f4be09af0a9d
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-06-14 12:48:21 -0700

    cleaned up notes to myself

--- app/js/entry.js
+++ app/js/entry.js
@@ -1,8 +1,5 @@
 require('angular/angular');
 var angular = window.angular;
 
-//
 var calculatorApp = angular.module('CalculatorApp', []);
-// require('./services/services')(bearStreamApp);
 require('./calculator/calculator')(calculatorApp);
-// require('./bears/bears')(bearStreamApp);
